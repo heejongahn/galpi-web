aa7bc1bc0ec69f27418d43911e7ca14f5ec99de3
Heejong Ahn <heejongahn@gmail.com>
2020-04-15 23:55:13 +0900

    Add `icon` prop to Badge atom

--- src/atoms/Badge/index.tsx
+++ src/atoms/Badge/index.tsx
@@ -1,19 +1,35 @@
 import React from 'react';
 import styled from '@emotion/styled';
+import Icon from '../Icon';
 
 interface Props {
   className?: string;
+  icon?: string;
   children: React.ReactNode;
 }
 
-export default function Badge({ className, children }: Props) {
-  return <Wrapper className={className}>{children}</Wrapper>;
+export default function Badge({ className, icon, children }: Props) {
+  return (
+    <Wrapper className={className}>
+      {icon != null ? <StyledIcon>{icon}</StyledIcon> : null}
+      {children}
+    </Wrapper>
+  );
 }
 
 const Wrapper = styled.span`
-  font-size: 0.5em;
+  display: flex;
+  align-items: center;
+
+  font-size: 0.75rem;
+  font-weight: 700;
 
   border: 1px solid black;
   border-radius: 2px;
   padding: 2px 6px;
+`;
+
+const StyledIcon = styled(Icon)`
+  font-size: 0.75rem;
+  margin-right: 4px;
 `;
