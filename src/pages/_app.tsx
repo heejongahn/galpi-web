7c860ab3769df592864e699c0266cf2a3441946b
Heejong Ahn <heejongahn@gmail.com>
2020-04-15 16:17:01 +0900

    Add global style and font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,23 @@
-import App from "next/app";
+import App from 'next/app';
+import Head from 'next/head';
 
-import "../styles/reset.scss";
+import '../styles/reset.scss';
 
 class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
 
-    return <Component {...pageProps} />;
+    return (
+      <>
+        <Head>
+          <link
+            rel="stylesheet"
+            href="//fonts.googleapis.com/css?family=Lato:300,300italic,400,400italic,700,700italic|Noto Serif KR:300,300italic,400,400italic,700,700italic&subset=latin,latin-ext"
+          />
+        </Head>
+        <Component {...pageProps} />
+      </>
+    );
   }
 }
 
